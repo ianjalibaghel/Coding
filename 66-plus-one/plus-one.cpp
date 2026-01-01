1c84cf51bfc22c564b8fd26670a67970669940ca
Anjali Baghel <137780664+ianjalibaghel@users.noreply.github.com>
2026-01-01 13:50:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (14.65%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        if(digits[n-1]!=9){
+            digits[n-1] +=1;
+            return digits;
+        }else{
+            for(int i=n-1;i>=0;i--){
+                if(digits[i]==9){
+                    digits[i]=0;
+                    if(i!=0 && digits[i-1]!=9){
+                        digits[i-1]+=1;
+                        return digits;
+                    }
+                }
+            }
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
