a51bfdbe31e84ba0fb7ab01d3c72c213b5aed11a
Anjali Baghel <137780664+ianjalibaghel@users.noreply.github.com>
2026-05-03 13:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (36.38%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size())   return false;
+        int n= s.size();
+        string ans=s+s;
+        if(ans.find(goal)==-1)
+            return false;
+        return true;
+    }
+};
